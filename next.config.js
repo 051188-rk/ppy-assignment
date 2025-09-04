884757fb999759210fed145b66b25045b0ca13b4
051188-rk <186922.rk@gmail.com>
2025-09-04 11:14:40 +0530

    android migration using capacitor done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Optional: Disables image optimization (useful for certain hosting setups)
+  // This is the crucial line that creates the 'out' folder.
+  output: 'export',
+
+  // This part is recommended for static exports to avoid errors with the Image component.
   images: {
     unoptimized: true,
   },
